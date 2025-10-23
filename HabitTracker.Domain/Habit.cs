4a5af38181f2b55e79fe6e652a6465e1bcd1ecee
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-10-23 10:10:37 +0200

    Struktur för klasserna i Domain-lagret

---
+++ HabitTracker.Domain/Habit.cs
@@ -0,0 +1,12 @@
+namespace HabitTracker.Domain;
+
+public class Habit
+{
+    // Skapar ett unikt ID för varje vana
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string Name { get; set; } = "";
+    public int TargetPerWeek { get; set; } // t.ex. 5 pomodoros
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+    // Om vanan är arkiverad eller inte
+    public bool IsArchived { get; set; }
+}
---
+++ HabitTracker.Domain/Session.cs
@@ -0,0 +1,15 @@
+namespace HabitTracker.Domain;
+
+public class Session
+{
+    // Skapar ett unikt ID för varje session
+    public Guid Id { get; init; } = Guid.NewGuid();
+    // ID för vanan som denna session tillhör
+    public Guid HabitId { get; init; }
+    public DateTime StartTime { get; init; }
+    public DateTime EndTime   { get; init; }
+
+    public int DurationMinutes => (int)(EndTime - StartTime).TotalMinutes;
+    // Om man vill anteckna något om sessionen, såsom "Kunde inte fokusera". Värdet kan vara null
+    public string? Notes { get; set; }
+}
